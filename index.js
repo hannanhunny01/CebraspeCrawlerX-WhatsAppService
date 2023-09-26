75f247ccf254c18e257aeeaae3cdec9dee73159d
hannanhunny01 <hannanhoney5000@gmail.com>
2023-09-25 21:17:45 -0300

    working on telegram services

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv').config();
-
+const {dbConnect} = require('./config/dbConnect')
 const PORT = process.env.PORT || 4000;
 const app = express();
+
+dbConnect();
+
 const message = require('./controller/message')
 const {sendMessagePas} = require('./controller/sendMessagePas')
 
@@ -12,6 +15,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // Define a simple "hello" route
+
+const bot = require('./controller/code/telegramCode')
 
 app.get('/sendHello',(req,res) =>{
     try{
