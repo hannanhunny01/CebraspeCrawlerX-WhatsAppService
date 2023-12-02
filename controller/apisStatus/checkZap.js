cca76c25d726b1ad7c0326cf63c09cc20fbe170b
hannanhunny01 <hannanhoney5000@gmail.com>
2023-12-02 16:49:57 -0300

    Performace Efficeient WhatsApp handler with commands on telegramBot

--- controller/apisStatus/checkZap.js
+++ controller/apisStatus/checkZap.js
@@ -9,7 +9,6 @@ const sendCheckMsg = async () => {
     const whatsappClient = await getWhatsappClient();
 
    const result =  await  whatsappClient.sendText(`5561986250932@c.us`, "Check Check Check")
-   console.log(result)
    return true;
 
   }catch(error){return false;};
--- controller/zapSession/session.js
+++ controller/zapSession/session.js
@@ -2,11 +2,54 @@ const { json } = require('body-parser');
 const venom = require('venom-bot');
 const fs = require('fs');
 const path = require('path');
+const photo = '../../out.png';
+const photoPath = path.join(__dirname, '../../out.png');
+
 
 const  { fileURLToPath } = require("url");
 
 
 let whatsappClient;
+
+const handleQRCode = async (base64Qr) => {
+  try {
+    const matches = base64Qr.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+    if (!matches || matches.length !== 3) {
+      throw new Error('Invalid input string');
+    }
+
+    const response = {};
+    response.type = matches[1];
+    response.data = Buffer.from(matches[2], 'base64');
+
+    const imageBuffer = response;
+
+    await new Promise((resolve, reject) => {
+      require('fs').writeFile(
+        'out.png',
+        imageBuffer['data'],
+        'binary',
+        (err) => {
+          if (err != null) {
+            console.log(err);
+            reject(err);
+          } else {
+            resolve();
+          }
+        }
+      );
+    });
+
+    // Now, call sendQrCode after the file is written
+    sendQrCode(photoPath);
+  } catch (error) {
+    throw error;
+  }
+};
+
+
+
+
 
 const initializeSession = () => {
   console.log("itcomes")
@@ -16,6 +59,8 @@ const initializeSession = () => {
         session: 'session-name',
         headless: false,
         autoClose: 60000,
+      }, (base64Qr, asciiQR, attempts, urlCode) => {
+        handleQRCode(base64Qr);
       })
       .then((client) => {
         whatsappClient = client;
@@ -47,11 +92,30 @@ const closeSession = async(req,res) =>{
 
 }
 
-const sendQrCode = async (req,res)=>{
 
-  return
+const sendQrCode = async (photoPath) => {
+  try {
+    const  telegramBot = require('../apisStatus/apiStatus')
 
-}
+    const photoBuffer = fs.readFileSync(photoPath);
+    telegramBot.sendMessage(process.env.TELEGRAM_CHAT_ID_APISTATUS, 'QrCode');
+    await telegramBot.sendPhoto(
+      process.env.TELEGRAM_CHAT_ID_APISTATUS,
+      photoBuffer,
+      { caption: 'QrCode' }
+    );
+   // fs.unlinkSync(photoPath);
+
+
+    console.log('Photo sent successfully.');
+  } catch (error) {
+    console.error('Error sending photo:', error);
+  }
+};
+
+//sendQrCode(photoPath);
+
+
 // Function to get the initialized whatsappClient
 const getWhatsappClient = async () => {
   try{
